202b69bc5372dcc4f4abf99952fb4c81331a7d87
VA-Alumno <ma.isamit@duocuc.cl>
2026-04-08 12:11:17 -0400

    creamos la exception inexistente

---
+++ atencion/src/main/java/com/hospitalVM/atencion/exception/MedicoInexistenteException.java
@@ -0,0 +1,8 @@
+package com.hospitalVM.atencion.exception;
+
+public class MedicoInexistenteException extends RuntimeException {
+    public MedicoInexistenteException(String message) {
+        super(message);
+
+    }
+}
