aca335c3b767724dfc372385c7edbee4eee8e8b7
VA-Alumno <ma.isamit@duocuc.cl>
2026-04-08 12:10:55 -0400

    creamos la ecception existente

---
+++ atencion/src/main/java/com/hospitalVM/atencion/exception/MedicoExistenteException.java
@@ -0,0 +1,8 @@
+package com.hospitalVM.atencion.exception;
+
+public class MedicoExistenteException extends RuntimeException {
+    public MedicoExistenteException(String message) {
+        super(message);
+
+    }
+}
